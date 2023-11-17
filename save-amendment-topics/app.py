45466ab746d1b37786818416a3e6e985e3cd1d58
Pedro Lucas Andrade <78179865+Pedrest15@users.noreply.github.com>
2023-11-17 09:03:16 -0300

    Add files via upload

---
+++ save-amendment-topics/app.py
@@ -0,0 +1,14 @@
+from controller.ControllerModelTopics import ControllerModelTopics
+from controller.ControllerConsultantTopics import ControllerConsultantTopics
+from doc.controller.controllerDoc import ControllerDoc
+from utils.initTables import initTables
+from server.instance import server
+
+if __name__ == "__main__":
+    initTables()
+
+    controllerConsultant = ControllerConsultantTopics(app=server.app,engine=server.engine)
+    controllerModel = ControllerModelTopics(app=server.app,engine=server.engine)
+    
+    controllerDoc = ControllerDoc(server.api)
+    server.start()
